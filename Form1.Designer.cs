dc03573f9f585aed9ed44fbc1dfe18ce12df8128
apple8803351 <apple8803351@gmail.com>
2016-12-25 19:35:17 +0800

    DBSCAN
    This is DBSCAN cluster

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DBSCAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.epsLabel = new System.Windows.Forms.Label();
+            this.minPtsLabel = new System.Windows.Forms.Label();
+            this.epsTextBox = new System.Windows.Forms.TextBox();
+            this.minPtsTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(13, 13);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(691, 472);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(714, 186);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(151, 60);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "選取資料集";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(714, 252);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(151, 60);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "執行";
+            this.runButton.UseVisualStyleBackColor = true;
+            // 
+            // epsLabel
+            // 
+            this.epsLabel.AutoSize = true;
+            this.epsLabel.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.epsLabel.Location = new System.Drawing.Point(710, 13);
+            this.epsLabel.Name = "epsLabel";
+            this.epsLabel.Size = new System.Drawing.Size(86, 24);
+            this.epsLabel.TabIndex = 3;
+            this.epsLabel.Text = "圓心半徑";
+            // 
+            // minPtsLabel
+            // 
+            this.minPtsLabel.AutoSize = true;
+            this.minPtsLabel.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.minPtsLabel.Location = new System.Drawing.Point(710, 107);
+            this.minPtsLabel.Name = "minPtsLabel";
+            this.minPtsLabel.Size = new System.Drawing.Size(86, 24);
+            this.minPtsLabel.TabIndex = 4;
+            this.minPtsLabel.Text = "範圍數量";
+            // 
+            // epsTextBox
+            // 
+            this.epsTextBox.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.epsTextBox.Location = new System.Drawing.Point(710, 40);
+            this.epsTextBox.Name = "epsTextBox";
+            this.epsTextBox.Size = new System.Drawing.Size(155, 33);
+            this.epsTextBox.TabIndex = 5;
+            this.epsTextBox.Text = "5";
+            // 
+            // minPtsTextBox
+            // 
+            this.minPtsTextBox.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.minPtsTextBox.Location = new System.Drawing.Point(710, 134);
+            this.minPtsTextBox.Name = "minPtsTextBox";
+            this.minPtsTextBox.Size = new System.Drawing.Size(155, 33);
+            this.minPtsTextBox.TabIndex = 6;
+            this.minPtsTextBox.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 497);
+            this.Controls.Add(this.minPtsTextBox);
+            this.Controls.Add(this.epsTextBox);
+            this.Controls.Add(this.minPtsLabel);
+            this.Controls.Add(this.epsLabel);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label epsLabel;
+        private System.Windows.Forms.Label minPtsLabel;
+        private System.Windows.Forms.TextBox epsTextBox;
+        private System.Windows.Forms.TextBox minPtsTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBSCAN
+{
+    public partial class Form1 : Form
+    {
+        List<MyPoint> points = new List<MyPoint>();
+
+        Bitmap bitmap;
+
+        int xMultiple, yMultiple;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            openFileButton.Click += new System.EventHandler(openFileButton_Click);
+            runButton.Click += new System.EventHandler(runButton_Click);
+
+            this.Load += new System.EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(canvas.Width, canvas.Height);
+        }
+
+        /// <summary>
+        /// 清理畫布
+        /// </summary>
+        /// <param name="graphics"></param>
+        private void clearGraphics(Graphics graphics)
+        {
+            graphics.Clear(Color.White);
+
+            canvas.Image = bitmap;
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            openFileDialog.Title = "請選擇資料集";
+
+            if (openFileDialog.ShowDialog(this) == DialogResult.Cancel)
+                return;
+
+            points.Clear();
+
+            clearGraphics(Graphics.FromImage(bitmap));
+
+            FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+
+            StreamReader streamReader = new StreamReader(fileStream);
+
+            //挑出資料集最大的X、Y軸 方便計算畫布與點的倍率
+            float maxX = 0;
+            float maxY = 0;
+
+            while (streamReader.Peek() != -1)
+            {
+                String[] str = streamReader.ReadLine().Split(' ');
+
+                //取得X軸
+                float x = float.Parse(str[0]);
+                //取得Y軸
+                float y = float.Parse(str[1]);
+
+                maxX = Math.Max(maxX, x);
+                maxY = Math.Max(maxY, y);
+
+                MyPoint myPoint = new MyPoint(x, y);
+
+                points.Add(myPoint);
+            }
+
+            //關閉檔案串流
+            fileStream.Close();
+
+            xMultiple = (int)(canvas.Width / maxX);
+            yMultiple = (int)(canvas.Height / maxY);
+
+            //圓的大小
+            int drawSize = 10;
+
+            //畫圓需要使用筆刷
+            SolidBrush solidBrush = new SolidBrush(Color.Blue);
+
+            //畫出資料點
+            for (int i = 0; i < points.Count; i++)
+            {
+                Graphics.FromImage(bitmap).FillEllipse(solidBrush, points[i].x * xMultiple - drawSize / 2, points[i].y * yMultiple - drawSize / 2, drawSize, drawSize);
+
+                canvas.Image = bitmap;
+            }
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            clearGraphics(Graphics.FromImage(bitmap));
+
+            //計算所有集合點 是否為 核心點、邊界點、雜訊點
+            for (int i = 0; i < points.Count; i++)
+            {
+                //因為要包含本身的點 所以預設為1
+                int number = 1;
+
+                /*
+                 * 用來記錄以圓為中心有包含住的點(不包括本身的點)
+                 * 假設這個點是核心點 那就放到MyPoint類別中的subPoints
+                 * 這樣做是方便尋找(密度直達)與(密度相連)的集合有哪些
+                 */
+                List<MyPoint> pointList = new List<MyPoint>();
+
+                //必須與其他不是本身的點做距離計算
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        //歐基里德距離公式  開根號((x1 - x2)平方 + (y1 - y2)平方)
+                        double distance = Math.Sqrt(Math.Pow(points[i].x - points[j].x, 2) + Math.Pow(points[i].y - points[j].y, 2));
+
+                        //如果發現在圓內或圓邊 則把number + 1; 並把點加入至pointList
+                        if (Double.Parse(epsTextBox.Text) >= distance)
+                        {
+                            number = number + 1;
+                            pointList.Add(points[j]);
+                        }
+                    }
+                }
+
+                //如果number >= minPtsTextBox 所設定的數量 代表此點為核心點
+                if (number >= Int16.Parse(minPtsTextBox.Text))
+                {
+                    //設定此點為核心點
+                    points[i].type = MyPoint.MAIN_POINT;
+
+                    for (int j = 0; j < pointList.Count; j++)
+                    {
+                        //如果點原本是雜訊點 則把點變成邊界點
+                        if (pointList[j].type == MyPoint.NOISE_POINT)
+                        {
+                            pointList[j].type = MyPoint.BORDER_POINT;
+                        }
+
+                        points[i].addMyPoint(pointList[j]);
+                    }
+
+                    //清除pointList的資料
+                    pointList.Clear();
+                }
+            }
+
+            //宣告(群數)變數 預設為0個群
+            int groupNumber = 0;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                //如果此點為核心點就執行
+                if (points[i].type == MyPoint.MAIN_POINT)
+                {
+                    //如果此核心點沒有貼上群組標籤 則執行DBSCAN遞迴
+                    if (points[i].groupID == 0)
+                    {
+                        cluster(points[i],groupNumber);
+
+                        //把目前的群數 +1
+                        groupNumber = groupNumber + 1;
+                    }
+                }
+            }
+
+            List<MyColor> groupColor = new List<MyColor>();
+
+            Random random = new Random();
+
+            //產生groupNumber族群的亂數顏色
+            for (int i = 0;i < groupNumber; i++)
+            {
+                int rColor = random.Next(0, 256);
+                int gColor = random.Next(0, 256);
+                int bColor = random.Next(0, 256);
+
+                groupColor.Add(new MyColor(rColor, gColor, bColor));
+            }
+
+            int drawSize = 10;
+
+            //繪製DBSCAN結果
+            for(int i = 0; i < points.Count; i++)
+            {
+                if(points[i].groupID == 0)
+                {
+                    SolidBrush solidBrush = new SolidBrush(Color.Blue);
+                    Graphics.FromImage(bitmap).FillEllipse(solidBrush, points[i].x * xMultiple - drawSize / 2, points[i].y * yMultiple - drawSize / 2, drawSize, drawSize);
+                }
+                else
+                {
+                    int rColor = groupColor[points[i].groupID - 1].rColor;
+                    int gColor = groupColor[points[i].groupID - 1].gColor;
+                    int bColor = groupColor[points[i].groupID - 1].bColor;
+
+                    SolidBrush solidBrush = new SolidBrush(Color.FromArgb(rColor, gColor, bColor));
+                    Graphics.FromImage(bitmap).FillEllipse(solidBrush, points[i].x * xMultiple - drawSize / 2, points[i].y * yMultiple - drawSize / 2, drawSize, drawSize);
+                }
+
+                canvas.Image = bitmap;
+            }
+        }
+
+        /// <summary>
+        /// DBSCAN 遞迴演算法
+        /// 利用陣列指標的方式來搜尋密度直接與密度相連的點有哪些
+        /// 並將這些點的groupID設定為同一個族群編號
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="groupNumber"></param>
+        private void cluster(MyPoint point, int groupNumber)
+        {
+            point.groupID = groupNumber + 1;
+
+            for (int i = 0; i < point.subPoints.Count; i++)
+            {
+                if (point.subPoints[i].type == MyPoint.MAIN_POINT)
+                {
+                    //檢驗族群編號是怕走過上一個核心點
+                    if (point.subPoints[i].groupID == 0)
+                    {
+                        cluster(point.subPoints[i], groupNumber);
+                    }
+                }
+                else
+                {
+                    if (point.subPoints[i].groupID == 0)
+                    {
+                        point.subPoints[i].groupID = groupNumber + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    class MyPoint
+    {
+        public const int NOISE_POINT = 1;
+        public const int BORDER_POINT = 2;
+        public const int MAIN_POINT = 3;
+
+        public float x;
+        public float y;
+        public int type;
+        public int groupID;
+
+        public List<MyPoint> subPoints = new List<MyPoint>();
+
+        public MyPoint(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+
+            //預設為雜訊點
+            this.type = NOISE_POINT;
+
+            //0為未分類點
+            this.groupID = 0;
+        }
+
+        /// <summary>
+        /// 如果為核心點 就把包含在圓心的點都放到subPoints
+        /// </summary>
+        /// <param name="point"></param>
+        public void addMyPoint(MyPoint point)
+        {
+            subPoints.Add(point);
+        }
+    }
+
+    class MyColor
+    {
+        public int rColor;
+        public int gColor;
+        public int bColor;
+
+        public MyColor(int rColor, int gColor, int bColor)
+        {
+            this.rColor = rColor;
+            this.gColor = gColor;
+            this.bColor = bColor;
+        }
+    }
+}
